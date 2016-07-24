764c04e2bc8a63d260291a5441b50654d0050083
Toma Luca <toma.luca95@gmail.com>
2016-07-24 18:25:05 +0200

    minor updates, fixed ui
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- modules/ve-speechToText/ve.ui.SpeechToTextDialog.js
+++ modules/ve-speechToText/ve.ui.SpeechToTextDialog.js
@@ -3,7 +3,9 @@ if (!mw.messages.exists('speechToText-ve-dialog-title')) {
         'speechToText-ve-dialog-title': 'Speach To Text',
         'speechToText-ve-dialog-add': 'Add text to the page',
         'speechToText-ve-dialog-cancel': 'Canecl',
+        'speechToText-ve-dialog-start': 'Start',
         'speechToText-ve-toolname': 'Speach to text',
+        'speechToText-ve-placeholder': 'Here will be the text...',
     });
 }
 
@@ -12,42 +14,54 @@ if ("webkitSpeechRecognition" in window) {
     webkitSpeechRecognitionOBJ.continuous = true;
     webkitSpeechRecognitionOBJ.interimResults = true;
 
-    webkitSpeechRecognitionOBJ.onstart = function() {}
+    webkitSpeechRecognitionOBJ.onstart = function() {
+        console.log("onstart");
+
+        var windowManager = ve.init.target.getSurface().getDialogs();
+        var speechToTextDialog = windowManager.getCurrentWindow();
+        speechToTextDialog.actions.setMode('running');
+    }
     webkitSpeechRecognitionOBJ.onresult = function(event) {
+        console.log("onresult");
         console.log(event);
         var windowManager = ve.init.target.getSurface().getDialogs();
         var speechToTextDialog = windowManager.getCurrentWindow();
         for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
-                /*$("#oojs-stt-final textarea").each(function(index) {
-                    console.log(index + ": " + $(this).text());
-                    current_txt = $(this).text().trim();
-                    new_recog_txt = event.results[i][0].transcript.trim() + ".";
-                    new_txt = current_txt + "\n" + new_recog_txt;
-                    $(this).text(new_txt.trim())
-                });*/
                 current_txt = speechToTextDialog.labelFinal.getValue().trim();
                 new_recog_txt = event.results[i][0].transcript.trim() + ".";
                 new_txt = current_txt + "\n" + new_recog_txt;
                 speechToTextDialog.labelFinal.setValue(new_txt.trim());
-                //$("#oojs-stt-partial").html("");
                 speechToTextDialog.labelPartial.setLabel("");
             } else {
                 speechToTextDialog.labelPartial.setLabel(event.results[i][0].transcript);
             }
         }
     };
-    webkitSpeechRecognitionOBJ.onerror = function(event) {}
-    webkitSpeechRecognitionOBJ.onend = function() {}
+    webkitSpeechRecognitionOBJ.onerror = function(event) {
+        console.log("onerror");
+        console.log(event);
+    }
+    webkitSpeechRecognitionOBJ.onend = function() {
+        console.log("onend");
+    }
 
     ve.ui.speechToTextDialog = function(manager, config) {
-        ve.ui.speechToTextDialog.parent.call(this, config);
+        ve.ui.speechToTextDialog.parent.call(this, manager, config);
     };
 
     OO.inheritClass(ve.ui.speechToTextDialog, OO.ui.ProcessDialog);
 
     ve.ui.speechToTextDialog.prototype.getBodyHeight = function() {
-        return 300;
+        return 320;
+    };
+
+    /* Set the default mode of the dialog */
+    ve.ui.speechToTextDialog.prototype.getSetupProcess = function(data) {
+        return ve.ui.speechToTextDialog.super.prototype.getSetupProcess.call(this, data)
+            .next(function() {
+                this.actions.setMode('intro');
+            }, this);
     };
 
     ve.ui.speechToTextDialog.static.name = 'speechToTextDialog';
@@ -57,13 +71,19 @@ if ("webkitSpeechRecognition" in window) {
         'action': 'add',
         'label': OO.ui.deferMsg('speechToText-ve-dialog-add'),
         'flags': ['primary', 'constructive'],
-        'modes': 'intro',
+        'modes': 'running',
         'icon': 'add'
     }, {
         'label': OO.ui.deferMsg('speechToText-ve-dialog-cancel'),
         'flags': 'safe',
-        'modes': 'intro',
+        'modes': ['intro', 'running'],
         'icon': 'close'
+    }, {
+        'action': 'start',
+        'label': OO.ui.deferMsg('speechToText-ve-dialog-start'),
+        'flags': ['primary', 'constructive'],
+        'modes': 'intro',
+        'icon': 'search'
     }];
 
     ve.ui.speechToTextDialog.prototype.initialize = function() {
@@ -92,28 +112,35 @@ if ("webkitSpeechRecognition" in window) {
         this.panel.$element.append(this.labelFinal.$element);
         this.panel.$element.append(this.labelPartial.$element);
         this.$body.append(this.panel.$element);
-        webkitSpeechRecognitionOBJ.start();
     };
 
     ve.ui.speechToTextDialog.prototype.getActionProcess = function(action) {
         var dialog = this;
         console.log("action: " + action);
         if (action === 'add') {
-            this.labelPartial.setLabel("ROBEBELLE");
-            //console.log(this.labelPartial.getValue());
-            new_text = null;
-            /*$("#oojs-stt-final textarea").each(function(index) {
-                new_text = $(this).text();
-                $(this).text("");
-            });*/
+            this.actions.setMode('intro');
 
             new_text = this.labelFinal.getValue().trim();
-            this.labelFinal.setValue("");
 
             var surfaceModel = ve.init.target.getSurface().getModel();
             var selection = surfaceModel.getSelection();
-            var range = selection.getRange();
-            surfaceModel.getFragment().adjustLinearSelection(range.end).collapseToStart().insertContent(new_text + "\n");
+            try {
+                var range = selection.getRange();
+
+                surfaceModel.getFragment().adjustLinearSelection(range.end).collapseToStart().insertContent(new_text + "\n");
+
+                this.labelPartial.setLabel("");
+                this.labelFinal.setValue("");
+            } catch (exc) {
+                alert("Something is wrong, sorry")
+            }
+            webkitSpeechRecognitionOBJ.stop();
+        } else if (action == 'start') {
+            webkitSpeechRecognitionOBJ.start();
+        } else {
+            this.actions.setMode('intro');
+
+            webkitSpeechRecognitionOBJ.stop();
         }
         return ve.ui.speechToTextDialog.super.prototype.getActionProcess.call(this, action);
     };
