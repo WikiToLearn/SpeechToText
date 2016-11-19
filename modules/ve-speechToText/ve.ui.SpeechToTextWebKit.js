35d94dd417f0cb75dac8e9909f2b5ba62ec61460
Toma Luca <toma.luca95@gmail.com>
2016-11-19 18:01:30 +0100

    added some try-catch
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- modules/ve-speechToText/ve.ui.SpeechToTextWebKit.js
+++ modules/ve-speechToText/ve.ui.SpeechToTextWebKit.js
@@ -5,37 +5,53 @@ if ("webkitSpeechRecognition" in window) {
     webkitSpeechRecognitionOBJ.lang = mw.config.get("wgContentLanguage");
 
     webkitSpeechRecognitionOBJ.onstart = function() {
-        var windowManager = ve.init.target.getSurface().getDialogs();
-        var speechToTextDialog = windowManager.getCurrentWindow();
-        speechToTextDialog.actions.setMode('running');
-        speechToTextDialog.stackLayout.setItem(speechToTextDialog.panel);
-        speechToTextDialog.labelFinal.setReadOnly(true);
+        try {
+            var windowManager = ve.init.target.getSurface().getDialogs();
+            var speechToTextDialog = windowManager.getCurrentWindow();
+            speechToTextDialog.actions.setMode('running');
+            speechToTextDialog.stackLayout.setItem(speechToTextDialog.panel);
+            speechToTextDialog.labelFinal.setReadOnly(true);
+        } catch (e) {
+            alert(e);
+        }
     }
     webkitSpeechRecognitionOBJ.onresult = function(event) {
-        var windowManager = ve.init.target.getSurface().getDialogs();
-        var speechToTextDialog = windowManager.getCurrentWindow();
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-                current_txt = speechToTextDialog.labelFinal.getValue().trim();
-                new_recog_txt = event.results[i][0].transcript.trim() + ".";
-                new_txt = current_txt + "\n" + new_recog_txt;
-                speechToTextDialog.labelFinal.setValue(new_txt.trim());
-                speechToTextDialog.labelPartial.setLabel("");
-            } else {
-                speechToTextDialog.labelPartial.setLabel(event.results[i][0].transcript);
+        try {
+            var windowManager = ve.init.target.getSurface().getDialogs();
+            var speechToTextDialog = windowManager.getCurrentWindow();
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    current_txt = speechToTextDialog.labelFinal.getValue().trim();
+                    new_recog_txt = event.results[i][0].transcript.trim() + ".";
+                    new_txt = current_txt + "\n" + new_recog_txt;
+                    speechToTextDialog.labelFinal.setValue(new_txt.trim());
+                    speechToTextDialog.labelPartial.setLabel("");
+                } else {
+                    speechToTextDialog.labelPartial.setLabel(event.results[i][0].transcript);
+                }
             }
+        } catch (e) {
+            alert(e);
         }
     };
     webkitSpeechRecognitionOBJ.onerror = function(event) {
-        var windowManager = ve.init.target.getSurface().getDialogs();
-        var speechToTextDialog = windowManager.getCurrentWindow();
-        speechToTextDialog.labelFinal.setReadOnly(false);
-        speechToTextDialog.actions.setMode('done');
+        try {
+            var windowManager = ve.init.target.getSurface().getDialogs();
+            var speechToTextDialog = windowManager.getCurrentWindow();
+            speechToTextDialog.labelFinal.setReadOnly(false);
+            speechToTextDialog.actions.setMode('done');
+        } catch (e) {
+            alert(e);
+        }
     }
     webkitSpeechRecognitionOBJ.onend = function() {
-        var windowManager = ve.init.target.getSurface().getDialogs();
-        var speechToTextDialog = windowManager.getCurrentWindow();
-        speechToTextDialog.labelFinal.setReadOnly(false);
-        speechToTextDialog.actions.setMode('done');
+        try {
+            var windowManager = ve.init.target.getSurface().getDialogs();
+            var speechToTextDialog = windowManager.getCurrentWindow();
+            speechToTextDialog.labelFinal.setReadOnly(false);
+            speechToTextDialog.actions.setMode('done');
+        } catch (e) {
+            alert(e);
+        }
     }
 }
